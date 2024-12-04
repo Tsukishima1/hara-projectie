4898e10f6024b3d994ad16de5d656403d588899a
Tsukishima1 <1732124764@qq.com>
2024-12-04 20:49:31 +0800

    feat: 增加日历视图组件

--- src/features/tasks/components/task-view-switcher.tsx
+++ src/features/tasks/components/task-view-switcher.tsx
@@ -10,11 +10,12 @@ import { DottedSeparator } from "@/components/dotted-seperator";
 import { Button } from "@/components/ui/button";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Loader, PlusIcon } from "lucide-react";
+import { TaskStatus } from "../types";
 import { useGetTasks } from "../api/use-get-tasks";
 import { useBulkUpdateTasks } from "../api/use-bulk-update-tasks";
 import { useTaskFilters } from "../hooks/use-task-filters";
 import { DataKanban } from "./data-kanban";
-import { TaskStatus } from "../types";
+import { DataCalendar } from "./data-calendar";
 
 export const TaskViewSwitcher = () => {
   const [{
@@ -87,8 +88,8 @@ export const TaskViewSwitcher = () => {
             <TabsContent value="kanban" className="mt-0">
               <DataKanban data={tasks?.documents??[]} onChange={onKanbanChange}/>
             </TabsContent>
-            <TabsContent value="calendar" className="mt-0">
-              {JSON.stringify(tasks)}
+            <TabsContent value="calendar" className="mt-0 h-full pb-4">
+              <DataCalendar data={tasks?.documents??[]}/>
             </TabsContent>
           </>
         )}
